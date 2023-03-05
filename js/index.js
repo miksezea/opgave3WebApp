abfbedd7602e0b8ad83c5c335876f24d81662edb
miksezea <mikk289e@edu.zealand.dk>
2023-03-05 15:24:29 +0100

    Before bootstrap

--- js/index.js
+++ js/index.js
@@ -6,6 +6,25 @@ Vue.createApp({
         }
     },
     methods: {
-        
+        submitWord() {
+            this.words.push({
+              original: this.inputWord,
+              lowercase: this.inputWord.toLowerCase(),
+              uppercase: this.inputWord.toUpperCase(),
+              capitalized: this.capitalize(this.inputWord),
+              endCapitalized: this.endCapitalize(this.inputWord),
+              reversed: this.reverse(this.inputWord)
+            })
+            this.inputWord = ''
+        },
+        capitalize(word) {
+            return word.charAt(0).toUpperCase() + word.slice(1)
+        },
+        endCapitalize(word) {
+            return word.slice(0, -1) + word.slice(-1).toUpperCase()
+        },
+        reverse(word) {
+            return word.split('').reverse().join('')
+        }
     }
 }).mount("#app")
